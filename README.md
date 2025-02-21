a551df9264a6e4ce35b8702c3e87130e613a2685
rust-jesty-dev <soljesty.dev@gmail.com>
2025-02-21 21:40:43 +0300

    add deployed contracts

--- README.md
+++ README.md
@@ -1,34 +1,66 @@
+
 # mns-contracts
+
 Monad Domains smart contracts
 
 ## Install
-to install dependencies, run the following command.
-```shell
-npm install 
-```
 
-## Set .env 
-Set you API keys on .env file. rename .env.template file as .env
+to install dependencies, run the following command.
+
 ```shell
+
+npm install
+
+``` 
+
+## Set .env
+
+Set you API keys on .env file. rename .env.template file as .env
+
+```shell
+
 ALCHEMY_API_KEY=
+
 ETHER_SCAN_API_KEY=
+
 DEPLOYER_PRIVATE_KEY=
+
 ```
 
 ## Compile
+
 ```shell
-npx hardhat compile --network sepolia
+
+npx  hardhat  compile  --network  sepolia
+
 ```
 
 ## Deploy
-to deploy contracts on Ethereum Sepolia testnet, run the following command. ensure your account have sufficient balance of ETH. 
+
+to deploy contracts on Ethereum Sepolia testnet, run the following command. ensure your account have sufficient balance of ETH.
+
 ```shell
-npx hardhat run scripts/deploy.js --network sepolia
+
+npx  hardhat  run  scripts/deploy.js  --network  sepolia
+
 ```
+
 
 ## Verfiy
-to verify your contracts on Ethereum Sepolia, you need to get your API key from etherscan.io and set your key on .env file 
+
+to verify your contracts on Ethereum Sepolia, you need to get your API key from etherscan.io and set your key on .env file
 
 ```shell
-npx hardhat verify --network sepolia {{CONTRACT ADDRESS}} {{CONTRACT DEPLOYMENT PARAMETERS}}
+npx  hardhat  verify  --network  sepolia  {{CONTRACT  ADDRESS}}  {{CONTRACT  DEPLOYMENT  PARAMETERS}}
 ```
+
+## Deployed Contracts
+|Contract| Monad Testnet | Monad Mainnet |
+|--|--|--|
+| MONRegistrarController | 0x04894Bf8F0ca6370C0fd8F5FCE3158033b46d979 | |
+| BaseRegistrarImplementation | 0x01BeCD733ea490CCDa8B5Caa97381E67BFA5249D | |
+| PublicResolver | 0x9F64406E0ef653074F8aF8178f0717e766A32764 | |
+| StablePriceOracle | 0x62375065222890a0Cc4FDD3D89c3dD3AC2850708 | |
+| Registry | 0x6442eC5c3CCDaF112d6B78F9189cD111d516fE1E | |
+| FIFSRegistrar | 0x84bceE5bB22ED7009dD8Af44fb4b74088c4F4C15 | |
+| ReverseRegistrar | 0x604bdEE35ebC13ef233f6b4e20613061fDf5C7E5 | |
---
+++ scripts/deployPriceOracle.js
@@ -0,0 +1,74 @@
+const {ethers, utils} = require("hardhat");
+require("@nomicfoundation/hardhat-ethers");
+ 
+const namehash = require('eth-ens-namehash');
+const tld = "mon"; 
+const labelhash = (label) => ethers.keccak256(ethers.toUtf8Bytes(label))
+const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+const ZERO_HASH = "0x0000000000000000000000000000000000000000000000000000000000000000";
+
+
+async function main() {
+ 
+   await deployWith();
+   
+};
+
+async function deploy (){
+  const MNSDeployer = await ethers.deployContract("MNSDeployer");
+  await MNSDeployer.waitForDeployment();
+  console.log(`MNSDeployer Deployed: ${MNSDeployer.target}`);
+} 
+
+async function deployWith() {
+  const [deployer] = await ethers.getSigners();
+  
+  //const stablePriceOracle = await deployStablePriceOracle();
+
+  
+  
+  const stablePriceOracle = await (await ethers.getContractFactory("StablePriceOracle")).attach("0x62375065222890a0Cc4FDD3D89c3dD3AC2850708")
+ 
+  await setupStablePriceOracle(stablePriceOracle, ethers.parseEther("0.000000000000001580"), ethers.parseEther("0.000000000000000316"), ethers.parseEther("0.000000000000000008"), ethers.parseEther("0.000000000000000004"), ethers.parseEther("0.000000000000000002"));
+
+
+   
+  const result = await stablePriceOracle.connect(deployer).price("1", 31536000);
+  console.log("Price Letter 1:"+  ethers.formatEther(result[0]));
+
+  const result2 = await stablePriceOracle.connect(deployer).price("22", 31536000);
+  console.log("Price Letter 2:"+  ethers.formatEther(result2[0]));
+
+  const result3 = await stablePriceOracle.connect(deployer).price("333", 31536000);
+  console.log("Price Letter 3:"+  ethers.formatEther(result3[0]));
+
+  const result4 = await stablePriceOracle.connect(deployer).price("4444", 31536000);
+  console.log("Price Letter 4:"+  ethers.formatEther(result4[0]));
+
+  const result5 = await stablePriceOracle.connect(deployer).price("55555", 31536000);
+  console.log("Price Letter 5:"+  ethers.formatEther(result5[0]));
+
+  const result6 = await stablePriceOracle.connect(deployer).price("666666", 31536000);
+  console.log("Price Letter 6:"+  ethers.formatEther(result6[0]));
+        
+}
+
+async function setupStablePriceOracle(stablePriceOracle, priceLetter1, priceLetter2, priceLetter3, priceLetter4, priceLetter5) {
+    await stablePriceOracle.setPrices([priceLetter1, priceLetter2, priceLetter3, priceLetter4, priceLetter5]);  
+    console.log(`Completed setupStablePriceOracle.`)
+  }
+
+async function deployStablePriceOracle() { 
+    const stablePriceOracle = await ethers.deployContract("StablePriceOracle");
+    await stablePriceOracle.waitForDeployment();
+    console.log(`StablePriceOracle Deployed: ${stablePriceOracle.target}`)
+    return stablePriceOracle;
+  }
+  
+ 
+
+  
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
